de5469aa839f7c7505b5b3b9db6d2306fcbc0897
aszed <ywm00@qq.com>
2019-05-10 09:00:45 +0800

    initialize project

---
+++ AbstractFactory/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author yuanweimin
+ * @date 19/05/10 08:59
+ * @description 
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
